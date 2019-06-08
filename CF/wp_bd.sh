cd5a63c232a48ae01bac8d4ef7d9c971d10b1e2d
Antonio Carlos <37047696+AnttoniC@users.noreply.github.com>
2019-06-08 19:09:22 -0300

    Update wp_bd.sh

--- CF/wp_bd.sh
+++ CF/wp_bd.sh
@@ -1,22 +1,26 @@
 #!/bin/bash 
 
-IP_PRIVATE=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+#IP_PRIVATE=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
 
 #Criando o Banco de Dados do WordPress
-
 sudo apt -y update
 
 sudo apt-get -y install mysql-server
 
-sudo apti -y install php libapache2-mod-php php-mysql
+sudo sed -i '43s/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+#sudo sed -i '43s/^/#/' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+sudo service mysqld restart
 
 sudo mysql <<EOF
 
 CREATE DATABASE wordpress;
 
-CREATE USER `wp_admin`@`$IP_PRIVATE` IDENTIFIED BY 'root';
+CREATE USER `wp_admin`@`$IP_WEB` IDENTIFIED BY 'root';
 
-GRANT ALL ON wordpress.* TO `wp_admin`@`$IP_PRIVATE`;
+GRANT ALL ON wordpress.* TO `wp_admin`@`$IP_WEB`;
+
+#GRANT ALL ON *.* TO 'wp_admin'@'$IP_WEB' IDENTIFIED BY 'root' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 EOF
