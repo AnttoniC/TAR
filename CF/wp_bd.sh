99e6ad191f9e12c26e3e665418d3f382f84c2847
Antonio Carlos <37047696+AnttoniC@users.noreply.github.com>
2019-06-10 18:54:28 -0300

    Update wp_bd.sh

--- CF/wp_bd.sh
+++ CF/wp_bd.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install mysql-server
 sudo sed -i '43s/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
 #sudo sed -i '43s/^/#/' /etc/mysql/mysql.conf.d/mysqld.cnf
 
-sudo service mysqld restart
+sudo service mysql restart
 
 sudo mysql <<EOF
 
@@ -20,7 +20,7 @@ CREATE USER `wp_admin`@`$IP_WEB` IDENTIFIED BY 'root';
 
 GRANT ALL ON wordpress.* TO `wp_admin`@`$IP_WEB`;
 
-#GRANT ALL ON *.* TO 'wp_admin'@'$IP_WEB' IDENTIFIED BY 'root' WITH GRANT OPTION;
+#GRANT ALL ON *.* TO 'root'@'$IP_WEB' IDENTIFIED BY 'root' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 EOF
