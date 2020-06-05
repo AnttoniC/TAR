171a1b4dbf779442fa43bcd3d44da171a39f2c18
Antonio Carlos <acsigt@gmail.com>
2020-06-05 09:00:51 -0300

    Update nfsServer.sh

--- Azure/Scripts NFS/nfsServer.sh
+++ Azure/Scripts NFS/nfsServer.sh
@@ -1,49 +1,21 @@
 #!/bin/bash
 
-ipS=""
-ipC=""
+IP=$(ip addr | grep "inet 10" > ip.txt && tr " " ":" < ip.txt > Ip.txt && cat Ip.txt | cut -d: -f6)
 
-read -p "Digite o IP do Servidor: " ipS
+#   Instalando NFS no Servidor
 
-read -p "Digute o IP do Cliente: " ipC
+sudo apt-get -y update
 
+sudo apt-get -y install nfs-kernel-server
 
-#ssh ubuntu@54.233.232.38 "echo claro >> algo.txt"
+#   Configurando a exportação do NFS no Servidor
 
-ssh-add Key_AWS_01.pem
+chmod 777 /etc/exports
 
-echo "Instalando NFS no Servidor"
+echo /home       $IP'(rw,sync,no_root_squash,no_subtree_check)' >> /etc/exports
+#echo /home       $ipC2'(rw,sync,no_root_squash,no_subtree_check)' >> /etc/exports
 
-ssh ubuntu@$ipS "sudo apt-get -y update"
-
-ssh ubuntu@$ipS "sudo apt-get -y install nfs-kernel-server"
-
-echo "Instalando NFS no Cliente"
-
-ssh ubuntu@$ipC "sudo apt-get -y update"
-
-ssh ubuntu@$ipC "sudo apt-get -y install nfs-common"
-
-echo "Configurando a exportação do NFS no Servidor"
-
-ssh ubuntu@$ipS "sudo chmod 777 /etc/exports"
-
-ssh ubuntu@$ipS "sudo echo /home       $ipC'(rw,sync,no_root_squash,no_subtree_check)' >> /etc/exports"
-
-ssh ubuntu@$ipS "sudo chmod 644 /etc/exports"
-
-ssh ubuntu@$ipS "sudo systemctl restart nfs-kernel-server"
-
-
-echo "Montando a pasta no Cliente"
-
-ssh ubuntu@$ipC "sudo mkdir -p /home"
-
-ssh ubuntu@$ipC "sudo mount $ipS:/home /home"
-
-echo "verificando montagem da pasta"
-
-ssh ubuntu@$ipC "df -h"
-
+chmod 644 /etc/exports
+systemctl restart nfs-kernel-server
 
 
