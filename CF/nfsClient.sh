d223a547e84a5f79fb4c30e0b5a4011c9932a5ea
Antonio Carlos <acsigt@gmail.com>
2020-06-15 15:56:42 -0300

    Update nfsClient.sh

--- CF/nfsClient.sh
+++ CF/nfsClient.sh
@@ -2,15 +2,16 @@
 
 #  Instalando NFS no Cliente
 
-sudo apt-get -y update
+sudo apt -y update
 
-sudo apt-get -y install nfs-common
+sudo apt -y install nfs-common
 
 #   Montando a pasta no Cliente
+#criando diretorio para /nfs/home para receber os arquivos compatilhado
+sudo mkdir -p /nfs/home
 
-sudo mkdir -p /home
-
-sudo mount $ipS:/home /home
+#Compartilhando o diretorio /home com os Clients no diretorio /nfs/home
+sudo mount $ipS:/home /nfs/home
 
 #  verificando montagem da pasta
 
