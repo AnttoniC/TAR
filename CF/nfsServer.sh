f26bdf900b9a65a6390e30726126e7612a9cf917
Antonio Carlos <acsigt@gmail.com>
2020-07-14 09:50:34 -0300

    Update nfsServer.sh

--- CF/nfsServer.sh
+++ CF/nfsServer.sh
@@ -13,7 +13,7 @@ export typeset ipSub=$(cat ip.txt | cut -d"." -f3)
 
 if [ $ipSub -eq 10 ];
 then
-        IP=10.0.0.0/24
+        IP=10.0.10.0/24
         echo $IP
 #elif [ $ipSub -eq 10 ];
 #then
